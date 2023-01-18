a002e065f06a3d8ad0b03693cde7a37e7bcaf444
unknown <brilight.shiner@gmail.com>
2023-01-18 13:27:55 +0100

    feat:Make index.html main.js App.vue

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+
+import Vue from 'vue';
+import App from './src/App.vue';
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
---
+++ src/component/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    Welcome to App Component.
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>

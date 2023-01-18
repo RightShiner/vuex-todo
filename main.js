26550560c32df7671aa57bb8073ed71734adbbe1
unknown <brilight.shiner@gmail.com>
2023-01-18 13:51:02 +0100

    feat:create Vuex store

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // main.js
 
 import Vue from 'vue';
-import App from './src/App.vue';
+import store from './store/store';
+import App from './App.vue';
 
-new Vue({
+const app = new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 });
